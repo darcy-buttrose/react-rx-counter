84c6c356986ce9a4c43d0b51f34ebbc83075c2af
darcy-buttrose <darcy.buttrose@gmail.com>
2016-04-25 20:30:19 +0800

    match cyclejs example

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,24 @@
 ///<reference path="./typings.d.ts" />
-import makeModel from './counter-model';
-import makeView from './counter-view';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import * as Rx from 'rx';
 import {component} from 'cycle-react';
 
-const Root = component(
-  'Root',
-  (interactions) => makeView(interactions, makeModel(interactions))
-);
+const Root = component('Root', (interactions) => {
+  let decrement$ = interactions.get('decrement').map(() => -1);
+  let increment$ = interactions.get('increment').map(() => +1);
+  let actions$ = Rx.Observable.merge(decrement$,increment$);
+  let counter$ = actions$.startWith(0).scan((x:number,y:number) => x+y);
+  
+  return counter$
+    .map((count) => (
+      <div>
+          <button onClick={interactions.listener('decrement')}>-</button>
+          {count}
+          <button onClick={interactions.listener('increment')}>+</button>
+      </div>
+    ));
+});
 
 ReactDOM.render(
   <Root />,
