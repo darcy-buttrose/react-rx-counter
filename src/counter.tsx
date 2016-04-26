615f1239cdf5ef93f0e5e1500b1d6703bf37f0f0
darcy-buttrose <darcy.buttrose@gmail.com>
2016-04-26 17:38:40 +0800

    multi counters

---
+++ src/counter.tsx
@@ -0,0 +1,23 @@
+///<reference path="./typings.d.ts" />
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import * as Rx from 'rx';
+import {component} from 'cycle-react';
+
+const Counter = component('Counter', (interactions) => {
+  let decrement$ = interactions.get('decrement').map(() => -1);
+  let increment$ = interactions.get('increment').map(() => +1);
+  let actions$ = Rx.Observable.merge(decrement$,increment$);
+  let counter$ = actions$.startWith(0).scan((x:number,y:number) => x+y);
+  
+  return counter$
+    .map((count) => (
+      <div>
+          <button onClick={interactions.listener('decrement')}>-</button>
+          {count}
+          <button onClick={interactions.listener('increment')}>+</button>
+      </div>
+    ));
+});
+
+export default Counter;
--- src/main.tsx
+++ src/main.tsx
@@ -3,24 +3,12 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as Rx from 'rx';
 import {component} from 'cycle-react';
-
-const Root = component('Root', (interactions) => {
-  let decrement$ = interactions.get('decrement').map(() => -1);
-  let increment$ = interactions.get('increment').map(() => +1);
-  let actions$ = Rx.Observable.merge(decrement$,increment$);
-  let counter$ = actions$.startWith(0).scan((x:number,y:number) => x+y);
-  
-  return counter$
-    .map((count) => (
-      <div>
-          <button onClick={interactions.listener('decrement')}>-</button>
-          {count}
-          <button onClick={interactions.listener('increment')}>+</button>
-      </div>
-    ));
-});
+import Counter from './Counter';
 
 ReactDOM.render(
-  <Root />,
+  <div>
+    <Counter />
+    <Counter />
+  </div>,
   document.querySelector('#app')
 );
